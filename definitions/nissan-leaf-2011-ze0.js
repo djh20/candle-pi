9d7cac481258c866ec10f902e0dd1497a190e52a
kobber-nz <danmanmrdj@gmail.com>
2022-01-27 16:37:47 +1300

    Decrease rate limit of rear_speed metric

--- definitions/nissan-leaf-2011-ze0.js
+++ definitions/nissan-leaf-2011-ze0.js
@@ -149,7 +149,7 @@ module.exports = {
           suffix: ' km/h',
           precision: 1,
           //log: true,
-          rateLimit: 80,
+          rateLimit: 50,
           // Increased division from 100 to 101 to decrease speed a bit.
           process: (data) => ((data[4] << 8) | data[5]) / 101
         },
