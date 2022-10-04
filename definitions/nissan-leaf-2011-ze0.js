a7668d3c258cd29f079f177ad8ad5123ec879fc6
djh20 <danmanmrdj@gmail.com>
2022-10-04 20:47:52 +1300

    Increase `wheel_speed` cooldown to reduce fluctuation

--- definitions/nissan-leaf-2011-ze0.js
+++ definitions/nissan-leaf-2011-ze0.js
@@ -190,7 +190,7 @@ module.exports = {
         {
           id: 'wheel_speed',
           precision: 2,
-          cooldown: 50,
+          cooldown: 100,
           defaultValues: [0, 0, 0],
           process: (data) => [ 
             ((data[4] << 8) | data[5]) / 100, // rear
