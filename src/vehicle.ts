dfc72c9969574585aaa989589d95604dc42fff97
djh20 <danmanmrdj@gmail.com>
2022-12-21 17:42:46 +1300

    Check if metric process function exists before calling it

--- src/vehicle.ts
+++ src/vehicle.ts
@@ -79,7 +79,9 @@ export default class Vehicle extends EventEmitter {
 
     topicDef.metrics.forEach(metricDef => {
       const metric = this.metrics.get(metricDef.id);
-      metric.update(metricDef.process(frame.data, this, metric.values));
+      if (metricDef.process) {
+        metric.update(metricDef.process(frame.data, this, metric.values));
+      }
       //logger.info("can", `${metricDef.id}: ${metric.value}`);
       //metric.instance.setValue( metric.process(frame.data) );
     });
